ff319e6266288d5f61c2b55d9e9576384229f569
Lancelei Herradura <lancelei@yelp.com>
2021-03-04 13:53:29 -0800

    Modifying test to only run process_datapoint w/ SignalFx generated metric w/ no try catch

--- tests/test_slo.py
+++ tests/test_slo.py
@@ -132,16 +132,10 @@ def test_SLODemultiplexer():
     assert demux.slo_watchers_by_label['slo_3'].process_datapoint.call_count == 1
     demux.slo_watchers_by_label['slo_3'].process_datapoint.reset_mock()
 
-    keyErrorRaised = False
-
-    try:
-        demux.process_datapoint(
-            props={'dimensions': {'sf_metric': '_SF_COMP_E123ABC'}}, datapoint=good, timestamp=0.0,
-        )
-    except KeyError:
-        keyErrorRaised = True
-
-    assert keyErrorRaised is False
+    # SignalFx generated metric ("_SF_COMP*") is discarded
+    demux.process_datapoint(
+        props={'dimensions': {'sf_metric': '_SF_COMP_E123ABC'}}, datapoint=good, timestamp=0.0,
+    )
 
     assert individual_slo_callback.call_count == 0
 
