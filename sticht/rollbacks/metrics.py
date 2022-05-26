921e79da3c9e37b5cae22c82bcbd559dd0797f77
Luis Perez <luisp@yelp.com>
2022-05-26 13:16:01 -0700

    Add scaffolding for creating metricwatchers
    wip

--- sticht/rollbacks/metrics.py
+++ sticht/rollbacks/metrics.py
@@ -1,9 +1,22 @@
+import logging
 import threading
 from typing import Any
 from typing import Callable
+from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Tuple
+from typing import Type
+from typing import TypeVar
+
+import yaml
+
+from sticht.rollbacks.soaconfigs import get_cluster_from_soaconfigs_filename
+from sticht.rollbacks.soaconfigs import get_rollback_files_from_soaconfigs
+
+T = TypeVar('T', bound='MetricWatcher')
+
+log = logging.getLogger(__name__)
 
 
 class MetricWatcher:
@@ -41,10 +54,35 @@ class MetricWatcher:
         """
         raise NotImplementedError()
 
+    @classmethod
+    def from_config(cls: Type[T], config: Dict[str, Any]) -> T:
+        raise NotImplementedError()
+
+
+def _get_metric_configs_for_service_by_cluster(service: str, soa_dir: str) -> Dict[str, Dict[str, Any]]:
+    return {
+        get_cluster_from_soaconfigs_filename(file): yaml.safe_load(file)
+        for file in get_rollback_files_from_soaconfigs(soa_dir)
+    }
+
 
 def watch_metrics_for_service(service: str, soa_dir: str) -> Tuple[List[threading.Thread], List[MetricWatcher]]:
     threads: List[threading.Thread] = []
     watchers: List[MetricWatcher] = []
 
-    # TODO: actually implement :)
+    for cluster, config in _get_metric_configs_for_service_by_cluster(service, soa_dir).items():
+        log.info(f'Processing configs for {service} in {cluster}...')
+
+        rollback_conditions = config.get('conditions')
+        if not rollback_conditions:
+            log.warning(f'{cluster} has a rollback file - but no conditions!')
+            continue
+
+        splunk_conditions = rollback_conditions.get('splunk')
+        if splunk_conditions:
+            log.info(f'Creating {len(splunk_conditions)} Splunk watchers...')
+            for splunk_rule in splunk_conditions:
+                log.info(f"Creating Splunk watcher for: {splunk_rule['label']}")
+                pass
+
     return threads, watchers
