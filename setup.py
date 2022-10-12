f360f98d8c4910bf41578c5a34b9f7142d3bca0b
Lancelei Herradura <lancelei@yelp.com>
2022-10-12 16:18:04 -0700

    Bump to v1.1.16

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='sticht',
-    version='1.1.15',
+    version='1.1.16',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
