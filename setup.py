f0e6f6836c7c0d5f73dbd659c427e34500bc7a9c
Luis Perez <luisp@yelp.com>
2025-03-27 09:03:08 -0700

    Release v1.2.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='sticht',
-    version='1.2.0',
+    version='1.2.1',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
