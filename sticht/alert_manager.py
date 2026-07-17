da7d187fcbc66f08d5e57592058a216a81aff676
Abu-Bakarr Jalloh <abubakarrj@yelp.com>
2026-07-17 05:48:53 -0700

    fix quotation marks

--- sticht/alert_manager.py
+++ sticht/alert_manager.py
@@ -36,42 +36,42 @@ class AlertmanagerClient:
         parameters: Dict[str, Union[str, Sequence[str]]] = {}
         # create filter parameter using specified filters
         if filters is not None:
-            parameters["filter"] = filters
+            parameters['filter'] = filters
         if params is not None:
             parameters.update(params)
 
         return parameters
 
     def _make_matchers_json(
-        self, filters: Optional[Sequence[str]]
+        self, filters: Optional[Sequence[str]],
     ) -> List[Dict[str, object]]:
         matchers: List[Dict[str, object]] = []
         if filters is None:
             return matchers
         for each_filter in filters:
-            if "!~" in each_filter:
-                key, value = each_filter.split("!~")
+            if '!~' in each_filter:
+                key, value = each_filter.split('!~')
                 is_regex = True
                 equals = False
-            elif "=~" in each_filter:
-                key, value = each_filter.split("=~")
+            elif '=~' in each_filter:
+                key, value = each_filter.split('=~')
                 is_regex = True
                 equals = True
-            elif "!=" in each_filter:
-                key, value = each_filter.split("!=")
+            elif '!=' in each_filter:
+                key, value = each_filter.split('!=')
                 is_regex = False
                 equals = False
-            elif "=" in each_filter:
-                key, value = each_filter.split("=")
+            elif '=' in each_filter:
+                key, value = each_filter.split('=')
                 is_regex = False
                 equals = True
             else:
                 raise AlertmanagerError(
-                    "Invalid filter",
-                    f"Filter: {each_filter!r}",
+                    'Invalid filter',
+                    f'Filter: {each_filter!r}',
                 )
             matchers.append(
-                {"isEqual": equals, "isRegex": is_regex, "name": key, "value": value}
+                {'isEqual': equals, 'isRegex': is_regex, 'name': key, 'value': value},
             )
         return matchers
 
@@ -80,28 +80,28 @@ class AlertmanagerClient:
         path: str,
         params: Optional[Mapping[str, Union[str, Sequence[str]]]] = None,
         json: Any = None,
-        request_type: str = "GET",
+        request_type: str = 'GET',
     ) -> Any:
         url = f"{self.alertmanager_url}/api/v2/{path.strip('/')}"
         headers = {
-            "User-Agent": "sticht",
+            'User-Agent': 'sticht',
         }
 
-        if request_type == "GET":
+        if request_type == 'GET':
             resp = requests.get(
                 url=url,
                 params=params,
                 headers=headers,
                 timeout=self.timeout,
             )
-        elif request_type == "POST":
+        elif request_type == 'POST':
             resp = requests.post(
                 url=url,
                 headers=headers,
                 json=json,
                 timeout=self.timeout,
             )
-        elif request_type == "DELETE":
+        elif request_type == 'DELETE':
             resp = requests.delete(
                 url=url,
                 headers=headers,
@@ -110,13 +110,13 @@ class AlertmanagerClient:
             return resp
         else:
             raise AlertmanagerError(
-                "Unknown request type",
-                f"Request type: {request_type!r}",
+                'Unknown request type',
+                f'Request type: {request_type!r}',
             )
         if resp.status_code != 200:
             raise AlertmanagerError(
-                f"Error while retrieving response from alertmanager: {resp.text}",
-                f"StatusCode: {resp.status_code!r}, Response: {resp.content!r}",
+                f'Error while retrieving response from alertmanager: {resp.text}',
+                f'StatusCode: {resp.status_code!r}, Response: {resp.content!r}',
             )
         return resp.json()
 
@@ -126,15 +126,14 @@ class AlertmanagerClient:
         params: Optional[Mapping[str, str]] = None,
     ) -> Any:
         return self._make_request(
-            path="/alerts", params=self._make_params(filters, params)
+            path='/alerts', params=self._make_params(filters, params),
         )
 
     def cluster_status(self) -> Any:
-        return self._make_request(path="/status")
+        return self._make_request(path='/status')
 
     def fetch_silences(
         self,
         filters: Optional[Sequence[str]] = None,
     ) -> Any:
-        return self._make_request(path="/silences", params=self._make_params(filters))
-
+        return self._make_request(path='/silences', params=self._make_params(filters))
